0b7e7f8e1d7cc01208be093a9d42e5db103ea547
Luis <ldorizsalazar@gmail.com>
2022-11-20 17:47:31 +0100

    add address

--- src/App.js
+++ src/App.js
@@ -11,21 +11,17 @@ const App = () => {
     <BrowserRouter style={{ margin: '150px' }}>
       <div style={{ height: 100 }}>
         <div id="nav" className="nav">
-          <Row
-          align="middle"
-          justify="space-between"
-
-          >
-            <Col className="logoContainer" lg={4} md={6} xs={9}>
+          <Row align="middle" justify="space-between">
+            <Col className="logoContainer" lg={4} md={6} sm={10} xs={18}>
               <NavLink exact="true" to="/">
                 <img className="logoImg" src={logo} alt="logo" />
               </NavLink>
             </Col>
-            <Col>
-              <Row justify='end'>
-                <a href="tel:+528120354817">Tel. 8120354817</a>
+            <Col lg={14} md={12} sm={10} xs={4}>
+              <Row justify="end">
+                <a href="tel:+528120351847">Tel. 8120351847</a>
               </Row>
-              <Row justify='end'>
+              <Row justify="end">
                 <NavLink exact="true" to="/terms">
                   Términos y condiciones
                 </NavLink>
@@ -35,6 +31,25 @@ const App = () => {
         </div>
       </div>
       <Routes />
+      <Row
+        justify="center"
+        className="banner"
+        align="middle"
+        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+      >
+        <Col span={20}>
+          <h3>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://goo.gl/maps/sqiw3uLuL3CM4pNAA"
+            >
+              Direccion: Edificio A piso 5 Av. Ricardo Margain Zozaya 555,
+              Corporativo Santa Engracia 1er Sector, 66273 Monterrey, N.L.
+            </a>
+          </h3>
+        </Col>
+      </Row>
     </BrowserRouter>
   );
 };
